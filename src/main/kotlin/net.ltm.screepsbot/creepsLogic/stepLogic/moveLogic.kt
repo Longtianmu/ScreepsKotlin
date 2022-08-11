7e013471d5b85d70b8ecee14e9e381e4782d0a94
Longtianmu <longtianmu@outlook.com>
2022-08-11 17:22:07 +0800

    ReConfig Path Reuse

--- src/main/kotlin/net.ltm.screepsbot/creepsLogic/stepLogic/moveLogic.kt
+++ src/main/kotlin/net.ltm.screepsbot/creepsLogic/stepLogic/moveLogic.kt
@@ -23,7 +23,7 @@ fun stepMove(creep: Creep): StepReturnCode {
             return StepReturnCode.SKIP_TICK
         }
         ALGORITHM_ASTAR
-        when (creep.moveTo(target, options { reusePath = 2 })) {
+        when (creep.moveTo(target)) {
             OK -> StepReturnCode.STATUS_IN_PROGRESS
             ERR_NO_PATH -> {
                 creep.memory.taskRetry++
